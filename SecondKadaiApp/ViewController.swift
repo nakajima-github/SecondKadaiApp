84d4c21c171e5ad8907650566ac2d6aec1e19f24
Nakajima <n_j5192@yahoo.co.jp>
2022-05-02 17:49:51 +0900

    First Commit

--- SecondKadaiApp/ViewController.swift
+++ SecondKadaiApp/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var username: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // segueから遷移先のResultViewControllerを取得する
+        let resultViewController:ResultViewController = segue.destination as! ResultViewController
+        
+        // 遷移先ResultViewControllerで宣言しているuserNameに値を代入する
+        resultViewController.userName =  username.text!
+    }
 
+    @IBAction func unwind(_ segue: UIStoryboardSegue) {
+        
+    }
 
 }
 
